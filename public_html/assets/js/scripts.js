e6c4823f9e1d94441ba7cc042905a821acf8e751
kkkiko <popo9855@g.skku.edu>
2022-06-22 22:24:38 +0900

    Add major selection button
    Add major selection button

---
+++ public_html/assets/js/scripts.js
@@ -0,0 +1,24 @@
+// // asd
+// const dropdown_item = document.querySelector(".dropdown-item");
+
+// dropdown_item.addEventListener("click", function () {
+//   alert("Click!");
+// });
+
+// const title = document.querySelector(".mainTitle");
+// function setTitle(major) {
+//   title.innerHTML = major + "<br/> Major Helper";
+// }
+
+// const software = document.querySelector(".software");
+// const business = document.querySelector(".business");
+// const economics = document.querySelector(".economics");
+// const engineering = document.querySelector(".engineering");
+// const mathematics = document.querySelector(".mathematics");
+// const medicine = document.querySelector(".medicine");
+// software.addEventListener("click", setTitle("software"));
+// business.addEventListener("click", setTitle("business"));
+// economics.addEventListener("click", setTitle("economics"));
+// engineering.addEventListener("click", setTitle("engineering"));
+// mathematics.addEventListener("click", setTitle("mathematics"));
+// medicine.addEventListener("click", setTitle("medicine"));
